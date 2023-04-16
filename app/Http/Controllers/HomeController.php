1262e2be1c2adc697db9806eaeac2934853241fa
Houssem Eddine Dahel <houssemoodahel@gmail.com>
2023-04-16 20:17:37 +0100

    FIX VIDEO POPUP

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,12 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Site_management;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
     public function index()
     {
-        return view('home');
+        $images = Site_management::first();
+        return view('home', [
+            'images' => $images,
+        ]);
     }
 }

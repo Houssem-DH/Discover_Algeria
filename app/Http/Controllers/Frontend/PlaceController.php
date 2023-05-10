1a4b702141ca1dcb7c89bcce032987cc2dcab11c
Houssem Eddine Dahel <houssemoodahel@gmail.com>
2023-05-10 15:31:09 +0100

    LAST COMMIT

--- app/Http/Controllers/Frontend/PlaceController.php
+++ app/Http/Controllers/Frontend/PlaceController.php
@@ -14,19 +14,39 @@ class PlaceController extends Controller
     public function view($slug)
     {
         
-        $place=Place::where('slug', $slug)->first();
-        $pgrequestc=Pgrequest::where('user_id', Auth::user()->id)->where('place_id', $place->id)->where('status', '0')->count();
-        $review=Review::where('place_id', $place->id)->orderBy('created_at', 'DESC')->paginate('8');
-        $countr=Review::where('place_id', $place->id)->count();
+        
 
+        if(Auth::check()){
+            $place=Place::where('slug', $slug)->first();
+        
+            $pgrequestc=Pgrequest::where('user_id', Auth::user()->id)->where('place_id', $place->id)->where('status', '0')->count();
+            $review=Review::where('place_id', $place->id)->orderBy('created_at', 'DESC')->paginate('8');
+            $countr=Review::where('place_id', $place->id)->count();
 
+            return view('Frontend.Places.view', [
+                'place' => $place,  
+                'review' => $review, 
+                'countr' => $countr,     
+                'pgrequestc' => $pgrequestc,     
+            ]);
+
+        } else{
+            $place=Place::where('slug', $slug)->first();
+        
+            
+            $review=Review::where('place_id', $place->id)->orderBy('created_at', 'DESC')->paginate('8');
+            $countr=Review::where('place_id', $place->id)->count();
+
+            return view('Frontend.Places.view', [
+                'place' => $place,  
+                'review' => $review, 
+                'countr' => $countr,     
+              
+            ]);
+
+        }
 
         
-        return view('Frontend.Places.view', [
-            'place' => $place,  
-            'review' => $review, 
-            'countr' => $countr,     
-            'pgrequestc' => $pgrequestc,     
-        ]);
+        
     }
 }
